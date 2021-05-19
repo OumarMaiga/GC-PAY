a3ebd03eeaad5b521cc25ba6941eb3c3e9905581
Hasseye <oumarm611@gmail.com>
2021-05-19 14:50:52 +0000

    Configuration pour l'envoi de mail

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -2,13 +2,9 @@
     <x-auth-card>
    
         <div class=" min-h-screen">
-          
-        
         <div class="auth-title">
-        MOT DE PASSE OUBLIE
-                </div>
-
-        
+            MOT DE PASSE OUBLIE
+        </div>
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
@@ -21,18 +17,15 @@
 
             <!-- Email Address -->
             <div class="form-group">
-             
-                        <input id="email" class="input-custom" type="text" name="email" value="{{ old('telephone') ?: old('email') }}" placeholder="EMAIL / TELEPHONE" required />
-                    </div>
-
-           
-                    <div class="mt-4">
-                        <button type="submit" class="btn-custom">
-                            {{ __('ENVOYER') }}
-                        </button>
-                    </div>
+                <input id="email" class="input-custom" type="text" name="email" value="{{ old('telephone') ?: old('email') }}" placeholder="EMAIL / TELEPHONE" required />
+            </div>
+    
+            <div class="mt-4">
+                <button type="submit" class="btn-custom">
+                    {{ __('ENVOYER') }}
+                </button>
+            </div>
          </div>
-        
         
         </form>
     </x-auth-card>
