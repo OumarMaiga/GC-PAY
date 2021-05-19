a8966f8fcccdd765ae6fbfe2e8bdcd6579c7aedf
Hasseye <oumarm611@gmail.com>
2021-05-19 15:10:26 +0000

    Implementation de changer de mot de passe

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,48 +1,46 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                
+                <div class="auth-title">
+                    CHANGER DE MOT DE PASSE
+                </div>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                <!-- Validation Errors -->
+                <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
+                <form method="POST" action="{{ route('password.update') }}">
+                    @csrf
 
-            <!-- Password Reset Token -->
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                    <!-- Password Reset Token -->
+                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+                    <!-- Email Address -->
+                    <div class="form-group">
+                        <input id="email" class="input-custom" type="email" name="email" value="{{ old('email', $request->email) }}" placeholder="EMAIL" required autofocus />
+                    </div>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+                    <!-- Password -->
+                    <div class="mt-4 form-group">
+                        <input id="password" class="input-custom" type="password" name="password" placeholder="MOT DE PASSE" required />
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div class="mt-4 form-group">
+
+                        <input id="password_confirmation" class="input-custom"
+                                            type="password"
+                                            name="password_confirmation" placeholder="CONFIRMATION DE MOT DE PASSE" required />
+                    </div>
+
+                    <div class="mt-4">
+                        <button type="submit" class="btn-custom">
+                            {{ __('CHANGER') }}
+                        </button>
+                    </div>
+                </form>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Reset Password') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
+        </div>
+    </div>
 </x-guest-layout>
