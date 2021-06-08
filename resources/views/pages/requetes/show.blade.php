5dd91252fde8d251a8f10d36e0ca3c2d8fe0a5da
sidiber <rokiatourose@gmail.com>
2021-06-08 16:31:30 +0200

    Design

--- resources/views/pages/requetes/show.blade.php
+++ resources/views/pages/requetes/show.blade.php
@@ -2,47 +2,86 @@
     <div class="container dashboard-content">
         <div class="row">
             <div class="show-title">
-                {{ $requete->slug }}
+                {{ $service->libelle }}
             </div>
         </div>
         <div class="row">
-            <div class="col-md-4 show-subtitle">
-                Etat: {{$requete->etat}}
+            <div class="col-md-4 show-detail souligne">
+                Code: 
             </div>
+            <div class="col-md-8 show-detail closer">
+            {{$requete->code}}
             </div>
+        </div>
         <div class="row">
-            <div class="col-md-4 show-subtitle">
-                Paiement
+            <div class="col-md-4 show-detail souligne">
+                Etat:
             </div>
-            <div class="col-md-8 show-detail">
-                @if ( $requete->paye == true)
-                    <span class="badge badge-success">Effecté</span>
+            <div class="col-md-8 show-detail closer">
+                @if ( $requete->etat == 'Terminé')
+                    <span class="badge badge-success padding">{{$requete->etat}}</span>
                 @else
-                    <span class="badge badge-danger">Non effecté</span>
-                @endif
+                    <span class="badge badge-info padding">{{$requete->etat}}</span>
+                @endif 
             </div>
             </div>
+            <div class="row">
+            @if($structure==NULL)
+            <div class="col-md-4 show-detail souligne">
+                Structure: 
+            </div>   
+            <div class="col-md-8 show-detail closer">
+                Non pécisé
+            </div>
+            @else
+            <div class="col-md-4 show-detail souligne">
+                Structure: 
+            </div> 
+            <div class="col-md-8 show-detail closer">
+                <a href="{{ route('structure.show', $structure->slug) }}">{{ $structure->libelle}}</a>
+            </div>
+            @endif
+        </div>
+        <div class="row">
+            <div class="col-md-4 show-detail souligne">
+                Prix: 
+            </div>
+            <div class="col-md-8 show-detail closer">
+            {{ $service->prix }}
+            </div>
+        </div>
         <div class="row">
             @if($service==NULL)
-                Service: Non pécisé
+            <div class="col-md-4 show-detail souligne">
+                Service: 
+            </div>   
+            <div class="col-md-8 show-detail closer">
+                Non pécisé
+            </div>
             @else
-                Service: <a href="{{ route('service.show', $service->slug) }}">{{ $service->libelle}}</a>
+            <div class="col-md-4 show-detail souligne">
+                Service: 
+            </div> 
+            <div class="col-md-8 show-detail closer">
+                <a href="{{ route('service.show', $service->slug) }}">{{ $service->libelle}}</a>
+            </div>
             @endif
+        </div>
+
+        <div class="row">
+            <div class="show-title">
+                Résumé
+            </div>
         </div>
         <div class="row">
-            @if($structure==NULL)
-                Structure: Non pécisée
-            @else
-                Structure: <a href="{{ route('structure.show', $structure->slug) }}">{{ $structure->libelle}}</a>
-            @endif
+            <div class="col-md-4 show-detail souligne">
+                Prix: 
+            </div>
+            <div class="col-md-8 show-detail closer">
+            {{ $service->prix }}
+            </div>
         </div>
-        <div class="row show-detail">
-            Effectué par: <i><a href="{{ route('admin.show', $user->email) }}"> {{ $user->prenom." ".$user->nom." (".$user->email.")" }}</a></i>
-           
-        </div>
-        <div class="row show-detail">
-            Code: {{$requete->code}}
-        </div>
+       
         @if ($requete->code == NULL)
             <div class="row col-md-4">
                 <div class="mt-4 row justify-content-center">
