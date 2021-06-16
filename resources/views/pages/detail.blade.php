5d0ac015256342d72e242078f53c359776ed7916
sidiber <rokiatourose@gmail.com>
2021-06-16 15:13:35 +0200

    Classe pour les span

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -8,8 +8,8 @@
         {{$service->description}}
     </div>
     <div class="show-detail padding-block">
-    <?php $structure = App\Models\Structure::where('id', $service->structure_id)->first() ?> 
-    <span style="text-decoration:underline; color:black">Durée</span>:  {{$service->duree}} <span style="text-decoration:underline; color:black;  padding-left:3%">Prix:</span>  {{$service->prix}}  <span style="text-decoration:underline; color:black;  padding-left:3%">Structure:</span>  {{$structure->libelle}}
+    <?php $structure = $service->structures()->first() ?> 
+    <span class="infos">Durée</span>:  {{$service->duree}} <span class="infos2">Prix:</span>  {{$service->prix}}  <span class="infos2">Structure:</span>  {{$structure->libelle}}
     </div>
     <div class=" padding-block">
             <h1 class=" text-blue-800 size">Formulaire à remplir</h1>
