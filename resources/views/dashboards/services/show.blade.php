02ceb7f29e005a7413bb2dbbd3aba3040c0a27b8
Hasseye <oumarm611@gmail.com>
2021-06-17 10:56:57 +0000

    Mise en page de sevice#show

--- resources/views/dashboards/services/show.blade.php
+++ resources/views/dashboards/services/show.blade.php
@@ -1,70 +1,33 @@
 <x-dashboard-layout>
     <div class="container dashboard-content">
-        <div class="row">
-            <div class="show-title">
-                {!! $service->libelle !!}
-            </div>
+        <div class="show-title-second">
+            {{ $service->libelle }}
         </div>
-        <div class="row">
-            <div class="col-md-4 show-subtitle">
-                Etat
-            </div>
-            <div class="col-md-8 show-detail">
-                @if ( $service->etat == true)
-                    <span class="badge badge-success">Acitve</span>
-                @else
-                    <span class="badge badge-danger">Inactive</span>
-                @endif
-            </div>
-            </div>
-        <div class="row">
-            <div class="col-md-4 show-subtitle">
-                Description: {{$service->description}}
-            </div>
-            </div>
-        <div class="row">
-            <div class="col-md-4 show-subtitle">
-                Prix: {{$service->prix}}
-            </div>
-            </div>
-        <div class="row">
-            <div class="col-md-4 show-subtitle">
-                Durée: {{$service->duree}}
-            </div>
-
+        
+        <div class="show-detail descript">
+            {{$service->description}}
         </div>
-        <div class="row">
-            @if($rubrique==NULL)
-                Rubrique: <a href="{{ route('service.edit', $service->slug) }}" class="text-blue-700">Ajouter</a>
-            @else
-                Rubrique: <a href="{{ route('rubrique.show', $rubrique->slug) }}">{{ $rubrique->libelle}}</a>
-            @endif
+    
+        <div class="show-detail">
+            <?php $structure = $service->structures()->first() ?>
+            <span class="infos">Durée</span>:  {{$service->duree}} <span class="infos2">Prix:</span>  {{$service->prix}}  <span class="infos2">Structure:</span><a href="{{ route('service.edit', $service->slug) }}">  {{ ($structure != null) ? $structure->libelle : ""}} </a>
         </div>
-        <div class="row">
-            @if($structure==NULL)
-                Structure: <a href="{{ route('service.edit', $service->slug) }}" class="text-blue-700">Ajouter</a>
-            @else
-                Structure: <a href="{{ route('structure.show', $structure->slug) }}">{{ $structure->libelle}}</a>
-            @endif
-        </div>
-        <div class="row show-detail">
-            Ajouté par: <i><a href="{{ route('admin.show', $user->email) }}"> {{ $user->prenom." ".$user->nom." (".$user->email.")" }}</a></i>
+    
+        <div class="show-detail mt-4">
+            <span class="infos">Ajouté par:</span> <i><a href="{{ route('admin.show', $user->email) }}"> {{ $user->prenom." ".$user->nom." (".$user->email.")" }}</a></i>
         </div>
         @if (Auth::user()->type == "admin-systeme")
-            <div class="row col-md-4">
-                <div class="mt-4 row justify-content-center">
+            <div class="mt-4">
+                <a href="{{ route('service.edit', $service->slug) }}"> <button class="mr-4 btn btn-outline-warning">MODIFIER</button></a>
 
-                    <a href="{{ route('service.edit', $service->slug) }}"> <button class="mr-4 btn btn-outline-warning">MODIFIER</button></a>
-
-                    <form  method="POST" action="{{ route('service.destroy', $service->id) }}">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="ml-4 btn btn-outline-danger" onclick="return confirm('Voulez-vous supprimer la structure ?')">
-                            RETIRER
-                        </button>
-                    </form>
-                </div>
-            </div>             
+                <form  method="POST" action="{{ route('service.destroy', $service->id) }}" class="d-inline">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="ml-4 btn btn-outline-danger" onclick="return confirm('Voulez-vous supprimer la structure ?')">
+                        RETIRER
+                    </button>
+                </form>
+            </div>
         @endif
     </div>
 </x-dashboard-layout>
--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -1,40 +1,42 @@
 <x-app-layout>
 
-<div class="show-title-second padding-block">
-            {{ $service->libelle }}
-        </div>
+    <div class="show-title-second padding-block">
+        {{ $service->libelle }}
+    </div>
     
     <div class="show-detail padding-block descript">
         {{$service->description}}
     </div>
+
     <div class="show-detail padding-block">
-    <?php $structure = $service->structures()->first() ?>
-    <span class="infos">Durée</span>:  {{$service->duree}} <span class="infos2">Prix:</span>  {{$service->prix}}  <span class="infos2">Structure:</span>  {{ ($structure != null) ? $structure->libelle : ""}}
+        <?php $structure = $service->structures()->first() ?>
+        <span class="infos">Durée</span>:  {{$service->duree}} <span class="infos2">Prix:</span>  {{$service->prix}}  <span class="infos2">Structure:</span>  {{ ($structure != null) ? $structure->libelle : ""}}
     </div>
+    
     <div class=" padding-block">
-            <h1 class=" text-blue-800 size">Formulaire à remplir</h1>
-            <form method="POST" action="{{ route('detail.store') }}">
-                    @csrf
-                    <div class="row padding-top">
-                        <div class="col-md-6 form-group">
-                           
-                            <input id="num-facture" class="input-custom" type="text" name="num-facture" value="{{ old('num-facture') }}" placeholder="Numéro de facture" required />
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6 form-group">
-                            
-                            <input id="montant" class="input-custom" type="text" name="montant" value="{{ old('montant') }}" placeholder="Montant" required />
-                        </div>
-                    </div>
-                    <div class="col-md-6 mt-4">
-                        <button type="submit" class="btn btn-custom">
-                            {{ __('SOUMETTRE') }}
-                        </button>
-                        <a href="{{ route('resume',$service->slug) }}" type="button" class="btn btn-custom-secondary">
-                            {{ __('ANNULER') }}
-                        </a>
-                    </div>
-                </form>
-        </div>
+        <h1 class=" text-blue-800 size">Formulaire à remplir</h1>
+        <form method="POST" action="{{ route('detail.store') }}">
+            @csrf
+            <div class="row padding-top">
+                <div class="col-md-6 form-group">
+                    
+                    <input id="num-facture" class="input-custom" type="text" name="num-facture" value="{{ old('num-facture') }}" placeholder="Numéro de facture" required />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 form-group">
+                    
+                    <input id="montant" class="input-custom" type="text" name="montant" value="{{ old('montant') }}" placeholder="Montant" required />
+                </div>
+            </div>
+            <div class="col-md-6 mt-4">
+                <button type="submit" class="btn btn-custom">
+                    {{ __('SOUMETTRE') }}
+                </button>
+                <a href="{{ route('resume',$service->slug) }}" type="button" class="btn btn-custom-secondary">
+                    {{ __('ANNULER') }}
+                </a>
+            </div>
+        </form>
+    </div>
 </x-app-layout>
