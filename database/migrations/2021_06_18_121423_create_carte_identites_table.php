747894bce08f3801a3146257b767af09edf8aa23
Hasseye <oumarm611@gmail.com>
2021-06-18 14:26:41 +0000

    Table carte d'identite ajouter

--- database/migrations/2021_06_18_121423_create_carte_identites_table.php
+++ database/migrations/2021_06_18_121423_create_carte_identites_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasseportsTable extends Migration
+class CreateCarteIdentitesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,19 @@ class CreatePasseportsTable extends Migration
      */
     public function up()
     {
-        Schema::create('passeports', function (Blueprint $table) {
+        Schema::create('carte_identites', function (Blueprint $table) {
             $table->id();
             $table->string('nom');
             $table->string('prenom');
+            $table->string('prenom_pere');
+            $table->string('prenom_mere');
+            $table->string('nom_mere');
             $table->string('adresse');
+            $table->string('profession');
             $table->string('date_naissance');
             $table->string('lieu_naissance');
-            $table->string('numero_nina');
+            $table->string('prenom_pere');
+            $table->string('prenom_pere');
             $table->timestamps();
         });
     }
@@ -32,6 +37,6 @@ class CreatePasseportsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('passeports');
+        Schema::dropIfExists('carte_identites');
     }
 }
