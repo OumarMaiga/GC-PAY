77daf26ba4a76a931c67bbfd9242fc168b8a4a94
sidiber <rokiatourose@gmail.com>
2021-06-23 16:30:18 +0200

    Ajout des champs page paiement

--- resources/views/pages/paiement.blade.php
+++ resources/views/pages/paiement.blade.php
@@ -7,12 +7,14 @@
   </div>
  
   <div class="row  justify-content-center ">
-
+  
 
 
   <div class="accordion padding-top-second col-md-4 " id="accordionExample">
-  
-    <div class="card">
+  <!--Ouverture du formulaire depuis ce niveau mais les champs seront mis dans les card-body -->
+  <form method="POST" action="">
+                    @csrf
+    <div class="card border-transparent">
       <div class="card-header " id="headingOne">
         <h5 class="mb-0">
           <input class="btn btn-link" type="radio" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"  name="paye"/>
@@ -23,11 +25,26 @@
       </div>
 
       <div id="collapseOne" class="collapse" aria-labelledby="headingOne" >
-        <div class="card-body">
-        </div>
+        <div class="card-body ">
+          <!-- champs du formulaire -->
+          <div class="form-group">      
+            <label for="numero_carte"> Numéro de la carte</label>
+            <input id="numero_carte" class="input-custom" type="text" name="numero_carte" value="{{ old('numero_carte')}}" placeholder="Numéro de la carte" required />
+                      
+          </div>
+          <div class="row">
+            <div class="col-md-6 form-group">
+              <label for="code_securite"> Code de sécurité</label>
+              <input id="code_securite" class="input-custom" type="text" name="code_securite" value="{{ old('code_securite')}}" placeholder="Code de sécurité" required />
+            </div>
+            <div class="col-md-6 form-group">
+              <label for="date_expiration"> Date d'expiration</label>
+              <input id="date_expiration" class="input-custom" type="text" name="date_expiration" value="{{ old('date_expiration')}}" placeholder="Date expiration" required />
+            </div>
+          </div>
       </div>
     </div>
-    <div class="card mt-3">
+    <div class="card mt-3 border-transparent">
       <div class="card-header" id="headingTwo">
         <h5 class="mb-0"> 
           <input class="btn btn-link collapsed" type="radio" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" name="paye"/>
@@ -37,11 +54,15 @@
       </div>
       <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" >
         <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          <!-- champs du formulaire -->
+          <div class="form-group">        
+            <label for="telephone"> Numéro de téléphone</label>
+            <input id="telephone" class="input-custom" type="text" name="telephone" value="{{ old('telephone')}}" placeholder="Numéro de téléphone" required />      
+          </div>
         </div>
       </div>
     </div>
-    <div class="card mt-3">
+    <div class="card mt-3 border-transparent">
       <div class="card-header" id="headingThree">
         <h5 class="mb-0">
           <input class="btn btn-link collapsed" type="radio" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"  name="paye"/>
@@ -50,11 +71,23 @@
         </h5>
       </div>
       <div id="collapseThree" class="collapse" aria-labelledby="headingThree" >
-        <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        <div class="card-body ">
+          <!-- champs du formulaire -->
+          <div class="form-group">         
+            <label for="telephone"> Numéro de téléphone</label>
+            <input id="telephone" class="input-custom" type="text" name="telephone" value="{{ old('telephone')}}" placeholder="Numéro de téléphone" required />              
+          </div>
         </div>
       </div>
     </div>
+    
+    <div class="mt-4 justify-content-center">
+                        <button type="submit" class="btn btn-custom full">
+                            {{ __('PAIEMENT') }}
+                        </button>
+    </div>
+              
+    </form>
   </div>
 </div>
 </x-app-layout>
