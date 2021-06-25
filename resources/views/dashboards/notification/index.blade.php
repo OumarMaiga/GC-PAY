2ce402ac6b11d9d9a23de49c13e29a7a9e39d276
Hasseye <oumarm611@gmail.com>
2021-06-25 08:48:59 +0000

    Notif correction

--- resources/views/dashboards/notification/index.blade.php
+++ resources/views/dashboards/notification/index.blade.php
@@ -6,11 +6,10 @@
                     <div class="list-group-item list-group-item-action <?= ($notification->vue == false) ? "list-group-item-unseen" : "" ?>">
                         <div class="row">
                             <div class="col-sm-10">
-                                    {!! $notification->description !!}
-                                </div>
-                                <div class="col-sm-2 d-flex justify-content-end">
-                                    {!! custom_date($notification->created_at) !!}
-                                </div>
+                                {!! $notification->description !!}
+                            </div>
+                            <div class="col-sm-2 d-flex justify-content-end">
+                                {!! custom_date($notification->created_at) !!}
                             </div>
                         </div>
                     </div>
